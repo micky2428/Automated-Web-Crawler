21944ebb548ff0ced9f0114b75e6b000949ada7c
Yuan-Ting Liang <99666209+micky2428@users.noreply.github.com>
2024-11-13 16:55:38 +0800

    Add files via upload

--- pchome-selenium-DealWithPages.py
+++ pchome-selenium-DealWithPages.py
@@ -18,7 +18,7 @@ import numpy as np
 import json
 from datetime import datetime
 
-##爬取標的和頁數
+##輸入要爬取的查核標的和頁數
 keyword = input("請輸入關鍵字")
 num_pages = int(input("請輸入要爬取的頁數"))
 ecode = 'utf-8-sig'
@@ -39,19 +39,33 @@ product_specs= []
 #BSMI
 bsmi_detect= []
 
-#headers
+#headers_home
+# my_headers = {
+#     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+# 'accept-encoding': 'gzip, deflate, br, zstd',
+# 'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+
+# 'sec-ch-ua': "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
+# 'sec-ch-ua-mobile': '?0',
+# 'sec-ch-ua-platform': "\"Windows\"",
+# 'sec-fetch-dest': 'document',
+# 'sec-fetch-mode': 'navigate',
+# 'sec-fetch-site': 'none',
+# 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+# 'x-api-source': 'pc',
+#       }
+
 my_headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 'accept-encoding': 'gzip, deflate, br, zstd',
 'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-
-'sec-ch-ua': "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
+'sec-ch-ua': "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
 'sec-ch-ua-mobile': '?0',
 'sec-ch-ua-platform': "\"Windows\"",
 'sec-fetch-dest': 'document',
 'sec-fetch-mode': 'navigate',
 'sec-fetch-site': 'none',
-'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
 'x-api-source': 'pc',
       }
 
@@ -68,7 +82,8 @@ time.sleep(5)
 
 # 開啟網頁
 print('---------- 開始進行爬蟲 ----------')
-tStart = time.time()#計時開始
+#開始計時
+tStart = time.time()
 
 #目標網址
 base_url = "https://24h.pchome.com.tw/search/?q=" + keyword
@@ -77,7 +92,7 @@ productp_page_url = "https://24h.pchome.com.tw"
     
 #翻頁功能
 for page in range(1, num_pages + 1):
-    # 更新 URL，將頁碼傳入 `p` 參數
+    # 更新 URL，將頁碼傳入參數"p"
     url = f"{base_url}&p={page}"
     driver.get(url)
     time.sleep(10)  # 等待頁面完全加載
@@ -85,13 +100,13 @@ for page in range(1, num_pages + 1):
     # 解析當前頁面的 HTML
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    # 提取當前頁面的商品連結和編號
+    # 爬取當前頁面的商品連結和編號
     for link_tag in soup.find_all("a", {"class": "c-prodInfoV2__link gtmClickV2"}):
         product_url = productp_page_url + link_tag.get("href")
         product_links.append(product_url)
         product_ids.append(link_tag.get("href").split("/")[-1])
 
-    # 可選：檢查是否有商品，若當前頁無商品則跳出循環
+    # 檢查是否有商品，若當前頁面無商品則跳出循環
     if not soup.find_all("a", {"class": "c-prodInfoV2__link gtmClickV2"}):
         print(f"第 {page} 頁無商品，停止爬取。")
         break
@@ -106,15 +121,16 @@ for link in product_links:
     # 解析商品頁面 HTML
     product_soup = BeautifulSoup(driver.page_source, 'html.parser')
     
-    # 根據商品頁面的 HTML 結構提取所需的資訊
+    # 根據商品頁面的 HTML 取所需的資訊
     # 品名
     product_name_tag = product_soup.find("h1", {"class": "o-prodMainName__grayDarkest o-prodMainName__grayDarkest--l700"})
     product_name = product_name_tag.text.strip() if product_name_tag else "未爬取到商品名稱"
     product_names.append(product_name)
 
     # 商品描述
-    # 使用 find_all 方法找到所有產品介紹的 <li> 元素
-    product_intro_tags = product_soup.find_all("li", {"class": "c-blockCombine__item c-blockCombine__item--prodSlogan"})
+    product_intro_tags_li = product_soup.find_all("li", {"class": "c-blockCombine__item c-blockCombine__item--prodSlogan"})
+    product_intro_tags_div = product_soup.find_all("div", {"class": "c-blockCombine c-blockCombine--prodSlogan"})
+    product_intro_tags = product_intro_tags_li+product_intro_tags_div
     product_intro = " ".join([tag.text.strip() for tag in product_intro_tags]) if product_intro_tags else "未爬取到商品描述"
     product_intros.append(product_intro)
     
@@ -138,11 +154,10 @@ for link in product_links:
     pattern = r'(M|R|D|T|Q)\s*\d{5}|(M|R|D|T|Q)\s*\d[A-Z]\d{3}|(M|R|D|T|Q)\d{2}[A-Za-z]\d{2}|(J)?C1\d{7}'
     matches = [word for word in words if re.match(pattern, word)]
 
-    # 如果沒有找到符合條件的匹配項，則設為 "未爬取到BSMI資訊"
+    # 如果沒有找到符合條件的，則設為 "未爬取到BSMI資訊"
     if not matches:
         matches = "未爬取到BSMI資訊"
 
-    # 將結果附加到 bsmi_detect 清單中
     bsmi_detect.append(matches)
     
     
@@ -168,8 +183,15 @@ print("下載了 {} 個商品".format(PRODUCT.shape[0]))
 current_time = datetime.now()
 formatted_time = current_time.strftime("%y%m%d_%H%M")
 NAME = f"{formatted_time}_Pchome_{keyword}.xlsx" 
+
+# 移除非法字符，例如 ASCII < 32 的字元
+def remove_illegal_chars(value):
+    if isinstance(value, str):
+        return re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\x9f]', '', value)
+    return value
+
+PRODUCT = PRODUCT.applymap(remove_illegal_chars)
 PRODUCT.to_excel(NAME, index=True)
-PRODUCT.to_excel(NAME)  # 將上面爬取的資訊存成.xlsx檔
 
 #計時結束
 tEnd = time.time()
